72f9961054961d5a1fc7e2565c9743cd9b9ef972
SebastianSi <sebastian.sirb90@gmail.com>
2019-06-21 14:54:41 +0300

    starting point

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import RepositoriesPage from './components/repositories/RepositoriesPage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="main-header">
+        App main header
       </header>
+      <RepositoriesPage />
     </div>
   );
 }
---
+++ src/components/repositories/RepositoriesPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function RepositoriesPage() {
+    return (
+        <h2>Repositories Page will be here</h2>
+    );
+}
+
+export default RepositoriesPage;
