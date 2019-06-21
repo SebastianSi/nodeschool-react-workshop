6c2e0fffdd1006780673a0a91b80454bf1a9e2cc
SebastianSi <sebastian.sirb90@gmail.com>
2019-06-21 17:20:56 +0300

    solution for milestone04 and task for milestone05

--- src/components/repositories/RepositoriesContainer.js
+++ src/components/repositories/RepositoriesContainer.js
@@ -4,6 +4,7 @@ import Repo from './Repo';
 class RepositoriesContainer extends React.Component {
 
     render() {
+        console.log(this.props.filters.searchText);
         return (
             <ul style={{listStyle: 'none'}}>
                 {/*Notice on the ul that React also allows style properties directly in JS*/}
--- src/components/repositories/RepositoriesPage.js
+++ src/components/repositories/RepositoriesPage.js
@@ -2,28 +2,29 @@ import React from 'react';
 import RepositoriesHeader from './RepositoriesHeader';
 import RepositoriesContainer from './RepositoriesContainer';
 
-function RepositoriesPage() {
-    return (
-        <div>
-            <RepositoriesHeader />
-            <hr/>
-            <RepositoriesContainer someProp={'Greetings from your parent'}/>
-        </div>
-    );
+class RepositoriesPage extends React.Component {
+
+    state = {
+        filters: {
+            searchText: 'SomeText'
+        }
+    };
+
+    //Note: the below function was added for milestone05
+    onSearchInputChange = valueFromChild => {
+        console.log(valueFromChild)
+    };
+
+    render() {
+        return (
+            <div>
+                <RepositoriesHeader onSearchInputChange={this.onSearchInputChange}/>
+                <hr/>
+                <RepositoriesContainer filters={this.state.filters}/>
+            </div>
+        );
+    }
+
 }
 
 export default RepositoriesPage;
-
-//TODO: Let's also refactor this to a class component. We want to do that because
-// we'll later get the value in the input in RepositoriesHeader and pass it to
-// RepositoriesContainer to only display based on the search filter; but later
-// on that. In the meantime, refactor this to a class component and also introduce
-// state to it; add a filters object on the state, which will have a searchText
-// property like this:
-// state = {filters: {
-//  searchText: 'SomeText'
-// }};
-// Now instead of someProp, pass the filters object on the state;
-// Make the necessaryChanges in RepositoriesContainer to console.log the searchText's value
-
-
